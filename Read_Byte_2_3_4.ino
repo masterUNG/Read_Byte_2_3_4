1305e8318fe5432b9b392f3c92be5593d44814a1
masterUNG <phrombutr@gmail.com>
2015-06-19 00:03:03 +0700

    fin

--- Read_Byte_2_3_4.ino
+++ Read_Byte_2_3_4.ino
@@ -1,12 +1,13 @@
 char BYTE;
-int led[] = {2,3,4};
+int led[] = {
+  2,3,4};
 
 void setup(){
 
   Serial.begin(9600);
-  
+
   for(int i=0; i<=2; i++){
-  pinMode(led[i], OUTPUT);
+    pinMode(led[i], OUTPUT);
   } //for
 
 } //setup
@@ -14,5 +15,38 @@ void setup(){
 void loop(){
 
   Serial.println("Please Type 2, 3, 4 or 0");
+  while (!Serial.available());
+  BYTE = Serial.read();
+  Serial.print(BYTE);
+  chooseLED(BYTE);
 
 } //loop
+
+void chooseLED(char myChar){
+
+  if(myChar == '2'){
+    digitalWrite(led[0], HIGH);
+  } //if2
+
+  if(myChar == '3'){
+    digitalWrite(led[1], HIGH);
+  } //if3
+
+  if(myChar == '4'){
+    digitalWrite(led[2], HIGH);
+  } //if4
+
+  if(myChar == '0'){
+    clearAll();
+  } //if0
+
+} //chooseLED
+
+void clearAll(){
+
+  for(int i=0; i<=2; i++){
+    digitalWrite(led[i], LOW);
+  } //for
+
+} //clearAll
+
